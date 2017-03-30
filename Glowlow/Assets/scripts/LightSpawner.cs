90472c69df66eab3f5bffcb7e65d73c68f157a84
frank <frankam@inbox.lv>
2017-03-30 22:07:07 +0200

    Level angelegt, Spieler kann sich schon bewegen und Lampen erzeugen

---
+++ Glowlow/Assets/scripts/LightSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSpawner : MonoBehaviour {
+
+	public float timeInterval;
+	public float lifeTime;
+	public Object light;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnLightsContinously());
+	}
+
+	IEnumerator SpawnLightsContinously(){
+		while(true)
+		{
+			//DoSomething();
+			Object newLight = Instantiate(light, transform.position, transform.rotation);
+			Destroy (newLight, lifeTime);
+			yield return new WaitForSeconds(timeInterval);
+		}
+	}
+}
---
+++ Glowlow/Assets/scripts/MoveWASD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWASD : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	public float rotSpeed;
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Vector3 temp = transform.position;
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(Vector3.back * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.down * Time.deltaTime * rotSpeed);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+		}
+	}
+}
