a50d7be8626b85c2ea01883844da555690259886
frank <frankam@inbox.lv>
2017-04-06 20:09:13 +0200

    Der Feind kann angreifen

---
+++ Glowlow/Assets/scripts/enemyAI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour
+{
+
+
+
+    public Transform target;
+    public float watchDistance;
+    public float followDistance;
+    public float attackDistance;
+    public float moveSpeed;
+    private Rigidbody rb;
+    public float damping;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        // print(distance);
+        if (distance < watchDistance)
+        {
+            watch();
+        }
+
+        if (distance < followDistance)
+        {
+            follow();
+        }
+
+        if (distance < attackDistance)
+        {
+            attack();
+        }
+
+    }
+
+    private void watch()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), damping);
+    }
+
+    private void follow()
+    {
+        // rb.AddRelativeForce(Vector3.forward * moveSpeed);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+
+    private void attack()
+    {
+        print("fight!");
+    }
+}
