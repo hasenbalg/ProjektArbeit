c94309778120dfcc02d8160f7e56e84877f4189d
frank <frankam@inbox.lv>
2017-05-04 09:39:58 +0200

    Energiequellen eingebaut

---
+++ Glowlow/Assets/scripts/PlaceEnergy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceEnergy : MonoBehaviour {
+
+	public GameObject energy;
+	public int howManyEnergy;
+
+	List<Vector3>possibleSpawnPoints;
+	// Use this for initialization
+	void Start () {
+		possibleSpawnPoints = new List<Vector3> ();
+
+		foreach(Transform child in transform){
+			possibleSpawnPoints.Add (child.position);
+		}
+
+		for(int i = 0; i < howManyEnergy; i++){
+			Instantiate (energy, possibleSpawnPoints[Random.Range (0, possibleSpawnPoints.Count)], Quaternion.identity * Quaternion.Euler(0,180,0));
+		}
+	}
+
+
+}
--- Glowlow/Assets/scripts/PlayerEnergy.cs
+++ Glowlow/Assets/scripts/PlayerEnergy.cs
@@ -6,19 +6,36 @@ public class PlayerEnergy : MonoBehaviour {
 
 
 	public Light playerLight;
-	private float energy = 1000;
+	public float fullEnergy = 1000f;
+	private float energy;
 	private float fullIntensity;
 
 	void Start(){
+		energy = fullEnergy;
 		fullIntensity = playerLight.intensity;
 	}
 
 
 	public void LooseEnergy (float damage) {
 		energy -= damage;
-		playerLight.intensity = fullIntensity * (energy / 1000);
+		playerLight.intensity = fullIntensity * (energy / fullEnergy);
 		if (energy == 0) {
 			Destroy (gameObject);
+		}
+	}
+
+	public void GainEnergy(){
+//		fill up to 100%
+		Debug.Log("Energy to gain: " + (energy - fullEnergy));
+		LooseEnergy (energy - fullEnergy);
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Energy")
+		{
+			GainEnergy ();
+			Destroy(col.gameObject);
 		}
 	}
 }
