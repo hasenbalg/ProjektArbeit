d2ac9952e26e3944a95414e8ebfa3e4cc952700c
Sebastian Matthiesen <info@sebastian-matthiesen.de>
2017-05-30 14:50:33 +0200

    Startbildschirm inkl. Funktion

---
+++ Glowlow/Assets/scripts/menuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuScript : MonoBehaviour {
+
+	public Button startText;
+	public Button settingsText;
+	public Button quitText;
+	public AudioClip impact;
+	AudioSource audio;
+
+	//public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+		startText = startText.GetComponent<Button>();
+		quitText = quitText.GetComponent<Button>();
+		audio = GetComponent<AudioSource>();
+
+	}
+
+	public void StartLevel(){
+		SceneManager.LoadScene ("level1");
+		print ("Start gedrückt");
+	}
+	public void QuitGame(){
+		Application.Quit ();
+		print ("Game beendet");
+	}
+
+	public void playHover(){
+		//hover.Play();
+
+		audio.PlayOneShot(impact, 0.7F);
+		print ("hoverd");
+		//hover.Play(44100);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
