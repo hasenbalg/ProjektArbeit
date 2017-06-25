7f3cddb999a71938ff0971efb7f14fabaef37d2a
frank <frankam@inbox.lv>
2017-06-25 11:19:06 +0200

    Steuerung ist jetzt geil

--- Glowlow/Assets/scripts/Player/Move.cs
+++ Glowlow/Assets/scripts/Player/Move.cs
@@ -36,7 +36,16 @@ public class Move : MonoBehaviour {
         // Movement
         CharacterController controller = GetComponent<CharacterController>();
 		if (controller.isGrounded) {
+			//moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			//move cams back and forth
+			foreach(Camera cam in Camera.allCameras){
+				Vector3 camPos = cam.transform.localPosition;
+				Vector3 camRot = cam.transform.localEulerAngles;
+				cam.transform.localPosition = Vector3.Lerp (camPos, new Vector3 (0, 0, Input.GetAxis ("Vertical")) * .5f, .5f);
+//				cam.transform.localRotation = Vector3.Lerp (camRot, new Vector3 (0, 0, Input.GetAxis ("Horizontal")) * .5f, .5f);
+				cam.transform.localRotation = Quaternion.Euler(0, 0, Input.GetAxis ("Horizontal"));
+			}
 			moveDirection = transform.TransformDirection (moveDirection);
 			moveDirection *= currentSpeed;
 
